678b38f4efe7b381e09ad45afb84cd9f3070fc21
cirpo <alessandro.cinelli@gmail.com>
2017-09-12 09:33:46 +0100

    removed mocha/chai, added Jest

--- JS/README.md
+++ JS/README.md
@@ -11,3 +11,5 @@ npm install
 ```sh
 npm test
 ```
+
+The test suite used is [Jest](https://facebook.github.io/jest/), `npm test` will run `jest --watch --coverage`
--- JS/test/app.test.js
+++ JS/test/app.test.js
@@ -1,17 +1,5 @@
-const app = require('../src/app');
+const app = require("../src/app");
 
-const should = require('chai').should();
-const expect = require('chai').expect;
-const assert = require('chai').assert;
-
-describe('app.js', function() {
-  describe('helloWorld()', function() {
-    it('should return the "hello world" string when invoking the function', function() {
-      let result = app.helloWorld();
-
-      assert.equal(result, 'hello world');
-      expect(result).to.equal('hello world');
-      result.should.equal('hello world');
-    });
-  });
+test("it should print hello world", () => {
+  expect(app.helloWorld()).toBe("hello world");
 });
