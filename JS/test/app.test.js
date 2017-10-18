41a871e3a8ddad5c5d795c4268a29ba156d2a9ed
Oliver Nural <Olly.nural@hotmail.co.uk>
2017-10-18 11:32:30 +0000

    first test added checking last number

--- JS/test/app.test.js
+++ JS/test/app.test.js
@@ -7,3 +7,7 @@ test("it should print hello world", () => {
 test("It should print 1 as first number", () => {
   expect(app.printNumbers()[0]).toBe(1);
 });
+
+test("It should print 100 as last number", () => {
+  expect(app.printNumbers().pop()).toBe(100);
+});
