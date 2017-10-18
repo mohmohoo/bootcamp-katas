dcec3f8933a431368cedded60a120b9c93ece425
Oliver Nural <Olly.nural@hotmail.co.uk>
2017-10-18 13:13:34 +0000

    test for buzz working

--- JS/src/app.js
+++ JS/src/app.js
@@ -11,10 +11,14 @@ module.exports = {
 function printNumbers() {
   let numbers = [];
 
-  for(let i = 1; i<=100; i++){
-    if (i % 3 == 0) {
+  for(let i = 1; i <= 100; i++){
+    if (i % 3 === 0) {
       numbers.push("fizz");
-    } else {
+    } 
+    else if (i % 5 === 0) {
+      numbers.push("buzz");
+    }
+    else {
       numbers.push(i);
     }
   }
