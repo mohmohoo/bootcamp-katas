06b2771505b107098721040af4996f740f73bfda
mohmohoo <mohmohoo85@gmail.com>
2018-03-06 15:49:57 +0000

    Added Palindrome

--- NET/Bootcamp_NET/Palindrome/Class1.cs
+++ NET/Bootcamp_NET/Palindrome/Class1.cs
@@ -1,8 +1,56 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Palindrome
 {
     public class Class1
     {
+        public static bool IsPalindrome(string value)
+        {
+            // If the input is empty it is not a palindrome
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return false;
+            }
+            
+            // If the string has a length of 1 character it is a palindrome
+            if (value.Length == 1)
+            {
+                return true;
+            }
+
+
+            var rgx = new Regex("[^a-zA-Z0-9]");
+            value = rgx.Replace(value, "").ToUpper();
+
+            var stack = new Stack<char>();
+            var len = value.Length;
+            if (value.Length % 2 == 1)
+            {
+                var index = ((len + 1) / 2) - 1;
+                value = value.Remove(index, 1);
+            }
+
+            len = value.Length;
+            
+            for (var i = 0; i < len; i++)
+            {
+                var charVal = value[i];
+                if (stack.TryPeek(out char tempChar))
+                {
+                    if (tempChar.Equals(charVal))
+                    {
+                        stack.Pop();
+                        continue;
+                    }
+
+                    stack.Push(charVal);
+                }
+            }
+
+            return stack.Count == 0;
+        }
     }
 }
--- NET/Bootcamp_NET/PalindromeTests/UnitTest1.cs
+++ NET/Bootcamp_NET/PalindromeTests/UnitTest1.cs
@@ -1,14 +1,23 @@
 using System;
 using Xunit;
+using Palindrome; 
 
 namespace PalindromeTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("madam m a d a m    ", true)]
+        [InlineData("", false)]
+        [InlineData("a", true)]
+        [InlineData("abba", true)]
+        [InlineData("  ", false)]
+        [InlineData("Abba", true)]
+        [InlineData("Abba?", true)]
+        public void Test1(string value, bool expected)
         {
-
+           var actual = Class1.IsPalindrome(value);
+            Assert.Equal(expected, actual);
         }
     }
 }
