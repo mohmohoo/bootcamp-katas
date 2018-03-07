e1ec5b7bcd56b049dff0a48b768863b3fd15dce0
mohmohoo <mohmohoo85@gmail.com>
2018-03-07 14:35:56 +0000

    Removed foreach

--- NET/Bootcamp_NET/StringCalculator/Class1.cs
+++ NET/Bootcamp_NET/StringCalculator/Class1.cs
@@ -1,36 +1,49 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace StringCalculator
 {
     public class Calculator
     {
+
         public static int Calculate(string str)
         {
             if (string.IsNullOrWhiteSpace(str))
             {
                 return 0;
             }
+            var integers = new List<int>();
 
-            var strings = str.Split(',').ToList();
-            var sum = 0;
+            var strings = str
+                .Split(',')
+                .ToList();
 
-            if (strings.Count != 2)
+            if (strings.Count != 2 || strings.Any(aChar => !int.TryParse(aChar, out int parsedValue)))
             {
                 return 0;
             }
             
-            foreach (var aChar in strings)
-            {
-                if (int.TryParse(aChar, out int parsedValue))
-                {
-                    sum += parsedValue;
-                    continue;
-                }
+            return strings
+                .Sum(aChar => int.Parse(aChar));
+            //var sum = 0;
 
-                return 0;
-            }
+            //if (strings.Count != 2)
+            //{
+            //    return 0;
+            //}
 
-            return sum;
+            //foreach (var aChar in strings)
+            //{
+            //    if (int.TryParse(aChar, out int parsedValue))
+            //    {
+            //        sum += parsedValue;
+            //        continue;
+            //    }
+
+            //    return 0;
+            //}
+
+            //return sum;
         }
     }
 }
